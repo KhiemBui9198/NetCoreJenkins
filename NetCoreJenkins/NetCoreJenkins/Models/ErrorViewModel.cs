864a256ea1bfabb4142710e053617781074349e8
KhiemBui9198 <chikhiemkm9198@gmail.com>
2023-04-20 23:50:28 +0700

    init code

---
+++ NetCoreJenkins/NetCoreJenkins/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NetCoreJenkins.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
